5fad8ceb129794b671bdfebc75be2868755a2f0a
marimontefusco <mari.montefusco@gmail.com>
2022-10-12 18:52:32 -0300

    usando clsx p criar condicionais de tamanho no Text

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -1,8 +1,26 @@
-import { Text } from './Text';
+import { Text, TextProps } from './Text';
+import { Meta, StoryObj } from '@storybook/react';
 
 export default {
     title: 'Components/Text',
     component: Text, 
+    args: {
+        children: 'Lorem ipsum.',
+    }
+
+} as Meta<TextProps>;
+
+/* Configurações de tamanho de fonte: */
+export const Default: StoryObj<TextProps> = {};
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
 };
 
-export const Default = {};
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+};
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,11 +1,30 @@
-interface TextProps {
+import { clsx } from 'clsx';
+
+export interface TextProps {
     size?: 'sm' | 'md' | 'lg';
+    children: string;
 }
 
-export function Text({ size = 'md' }: TextProps) {
+export function Text({ size = 'md', children }: TextProps) {
     return (
-        <h1 className="text-gray-100 font-sans">Hello world</h1>
+        <span 
+            className= {clsx(
+                'text-gray-100 font-sans',
+                {
+                    'text-xs': size === 'sm',
+                    'text-sm': size === 'md',
+                    'text-md': size === 'lg'
+                }
+            )}
+        >
+            { children }
+        </span>
     )
-}
+};
 
 /* size = 'md'  -> se não informar o tamanho da fonte, usar o padrão que é o md medium */
+
+/* Aplique a classe text-xs caso o tamanho pedido seja 'sm', 
+classe text-sm caso o tamanho pedido seja 'md', 
+classe text-md caso o tamanho pedido seja 'lg'
+*/
