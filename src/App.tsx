4d0b9ec9f843fcedbbe3e6c2ba29b00a8ef0e7a2
marimontefusco <mari.montefusco@gmail.com>
2022-10-12 17:27:27 -0300

    importando tokens - configurando cores e tamanho fonte

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export function App() {
     <>
       <h1 className="font-bold text-5xl text-violet-500">Hello world</h1>
 
-      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-700">Enviar</button>
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white">Enviar</button>
 
    </>
   )
