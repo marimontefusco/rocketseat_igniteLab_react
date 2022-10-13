55501841c3f8c1d779f49b7b6e39da5e365ac0fc
marimontefusco <mari.montefusco@gmail.com>
2022-10-13 19:52:36 -0300

    comp TextInput - incluindo input sem icone

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -12,7 +12,7 @@ export interface TextInputRootProps {
 function TextInputRoot(props: TextInputRootProps) {
     return (
         <div 
-        className='flex items-center gap-3 py-4 px-3 w-full rounded bg-gray-800 focus-within:ring-1 ring-cyan-500'
+        className='flex items-center gap-3 h-12 py-4 px-3 w-full rounded bg-gray-800 focus-within:ring-1 ring-cyan-500'
         >
             {props.children}
         </div>
