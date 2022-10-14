4c19b200313cbca0f8e3b2759a2ea37af618c1e1
marimontefusco <mari.montefusco@gmail.com>
2022-10-14 16:27:53 -0300

    main.cjs: corrigindo codigo viteFinal

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,10 +15,10 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: ( config, { configType }) => {
+  "viteFinal": ( config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/rocketseat_igniteLab_react/'
     }
     return config
-  }
+  },
 }
