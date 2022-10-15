41fe77c139e31a4402541a469cc0ec7a9bd12ade
marimontefusco <mari.montefusco@gmail.com>
2022-10-15 17:57:02 -0300

    adicionando className nos componentes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,23 @@
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     asChild?: boolean;
+    className?: string;
 }
 
-export function Button({children, asChild}: ButtonProps) {
+export function Button({children, asChild,className, ...props}: ButtonProps) {
     const Component = asChild ? Slot : 'button';
     
     return (
         <Component
             className= {clsx(
-                'bg-cyan-500 text-black text-sm font-semibold px-3 py-4 rounded w-full transition-colors hover:bg-cyan-300 focus:ring-1 ring-white'
+                'bg-cyan-500 text-black text-sm font-semibold px-4 py-3 rounded w-full transition-colors hover:bg-cyan-300 focus:ring-1 ring-white',
+                className,
             )}
+            {...props}
         >
             {children}
         </Component>
