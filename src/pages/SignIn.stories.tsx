513bd5f4fed2c473ddbed97f06701ca4a8e3fdb4
marimontefusco <mari.montefusco@gmail.com>
2022-10-15 18:29:37 -0300

    pages: adicionando componente e stories do SignIn

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from './SignIn';
+import { Meta, StoryObj} from '@storybook/react';
+
+export default {
+    title: 'Pages/Sign in',
+    component: SignIn,
+    args: {},
+    argTypes: {}
+} as Meta;
+
+export const Default: StoryObj = {};
+
