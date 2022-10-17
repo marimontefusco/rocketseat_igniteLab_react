1b016a10b27b22bcd5f60b7fa041ca43e39384e0
marimontefusco <mari.montefusco@gmail.com>
2022-10-17 12:45:22 -0300

    SignIn: Testes e simulacao de interacoes do usuario

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,5 +1,8 @@
-import { SignIn } from './SignIn';
 import { Meta, StoryObj} from '@storybook/react';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { SignIn } from './SignIn';
+
 
 export default {
     title: 'Pages/Sign in',
@@ -8,5 +11,24 @@ export default {
     argTypes: {}
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
 
+    //Testes: Simulação das Interações do usuário:
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'mariabiriba@gmail.com');
+        userEvent.type(canvas.getByPlaceholderText('Digite sua senha'), '12345678');
+
+        userEvent.click(canvas.getByRole('button'));
+
+        await waitFor(() => {
+            return expect(canvas.getByText('Login realizado!')).toBeInTheDocument(); 
+        }) 
+    }
+};
+
+//canvasElement() -> é o wireframe que mostra o nosso elemento
+//expect() -> o q espero que aconteça na interface depois que o usuário já completou os campos e clicou no bot -> ser redirecionado pra uma nova tela? Que o texto 'Login realizado!' apareça na minha tela? 
+//.toBeInTheDocument() -> esteja em tela
+//waitFor() -> cria um setInterval e fica em loop, rodando o código dentro dos () várias vezes até passar no teste
